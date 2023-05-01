12ed5565387908cb996616c6b57df20725aa10fb
Olumide Jones <olumideyjones@yahoo.com>
2023-05-01 14:41:10 +0200

    add to cart button added

--- main.js
+++ main.js
@@ -87,3 +87,20 @@ window.onclick = function(event) {
     modal_2.style.display = "none";
   }
 }
+
+/****** ADDtoCART ******/
+const button = document.querySelector(".addtocart");
+const done = document.querySelector(".done");
+console.log(button);
+let added = false;
+button.addEventListener('click',()=>{
+  if(added){
+    done.style.transform = "translate(-110%) skew(-40deg)";
+    added = false;
+  }
+  else{
+    done.style.transform = "translate(0px)";
+    added = true;
+  }
+    
+});
